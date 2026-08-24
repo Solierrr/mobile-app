ea6385b40b9c2e29a9e434e9aa31f80584398036
meguito <doasilva2009@hotmail.com>
2026-08-24 18:17:32 -0300

    fix: replace wildcard import and remove unused import

--- app/src/main/java/com/project/solaria_mobile/ui/theme/Theme.kt
+++ app/src/main/java/com/project/solaria_mobile/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.project.solaria_mobile.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
